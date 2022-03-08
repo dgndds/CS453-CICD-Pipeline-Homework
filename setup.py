d76deccc86a9069b47e2a4991efe6af2e9726299
dgndds <dogancan1998231@gmail.com>
2022-03-08 23:43:14 +0300

    test7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
-    name="CI/CD-Homework-Dictionary-dgndds",
+    name="CI-CD-Homework-Dictionary-dgndds",
     author="Muhammed Doğancan Yılmazoğlu",  # TODO: Write your name
     version="2.0.0",
     packages=["dictionary"],
@@ -10,5 +10,4 @@ setup(
         "requests>=2.23.0",
     ],
     python_requires=">=3.8",
-
 )
